0e22758530e72320350a5149864dab33d4c18938
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-04-18 18:44:25 -0300

    Added some tests

---
+++ test/fixture/nuxt.config.js
@@ -0,0 +1,7 @@
+import path from 'path'
+
+export default {
+	components: true,
+	modules: ['../../lib/module.js'],
+	target: 'static',
+}
---
+++ test/fixture/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+	<div class="epic">
+		<octicons />
+		<octicons name="home" />
+		<octicons name="home" size="16" />
+		<octicons name="patata" size="16" />
+	</div>
+</template>
