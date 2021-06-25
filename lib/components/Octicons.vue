2686ee9c2489c15cbd867c42c32cc073110842ee
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-06-25 19:38:29 -0300

    fixed error when trying to use a 16px icon with larger sizes

--- lib/components/Octicons.vue
+++ lib/components/Octicons.vue
@@ -3,7 +3,7 @@
 		version="1.1"
 		:width="size"
 		:height="size"
-		:viewBox="`0 0 ${size > 16 ? 24 : 16} ${size > 16 ? 24 : 16}`"
+		:viewBox="`0 0 ${height} ${height}`"
 		style="fill: currentColor;"
 		class="octicon"
 		:class="`octicon-${name}`"
@@ -25,11 +25,25 @@ export default {
 		},
 	},
 	computed: {
+		height() {
+			const size = parseInt(this.size) != NaN ? parseInt(this.size) : 24
+			const height = size > 16 ? 24 : 16
+			const name = this.$octicons[this.name] ? this.name : 'bug'
+			if (this.$octicons[name].heights[height]?.path) {
+				return height
+			} else {
+				return 16
+			}
+		},
 		icon() {
 			const size = parseInt(this.size) != NaN ? parseInt(this.size) : 24
 			const height = size > 16 ? 24 : 16
 			const name = this.$octicons[this.name] ? this.name : 'bug'
-			return this.$octicons[name].heights[height].path
+			if (this.$octicons[name].heights[height]?.path) {
+				return this.$octicons[name].heights[height].path
+			} else {
+				return this.$octicons[name].heights[16].path
+			}
 		},
 	},
 }
